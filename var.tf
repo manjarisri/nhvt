0e71216bab6b8bfeb673bfcf6c5c9ebf24d3bb9f
Manjari Srivastav <126674100+manjarisri@users.noreply.github.com>
2023-06-19 12:38:59 +0530

    Add files via upload

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "azurerm_subnet" {
+  description = "Name of Subnet"
+  type = string
+  default = "sub1"
+}
+
+variable "azurerm_public_ip" {
+    description = "Name of public Ip"
+  type = string
+  default = "terra-ip"
+  
+}
